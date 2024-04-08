c554e18e78a22dff313cbd18cf06a9716a3f2227
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-08 10:25:30 +0100

    first commit

---
+++ src/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+function About(){ 
+return(
+
+<div>
+
+    <div className="About">
+<p>Hello my name is Michael Moncayo. I AM FROM Ecuador.
+I am passionate about economics,finance and coding <br></br> and many other things. 
+<br></br>I study international bussines and my free time and build stuffs that i want. 
+You can see my work about coding here. <br></br>
+also i have my blog about economics.<br></br> please check and suscribe
+    fun fact  i speak 3 and half lenguage. this summer i take french.</p>
+
+
+
+
+    </div>
+
+
+
+</div>
+
+);
+
+}
+
+
+export default About;
